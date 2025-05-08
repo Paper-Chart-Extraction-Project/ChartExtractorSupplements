eeb3f85b1712194189ad2ea2af4a112e1b07fe1e
Ryan Folks <rfolks92@gmail.com>
2025-05-08 09:05:51 -0400

    Create optimize_cpd_parameters.py, a command line program that will find optimal parameters for the coherent point drift algorithm.

---
+++ utilities/optimization/optimize_cpd_parameters.py
@@ -0,0 +1,6 @@
+"""A command line program to optimize the parameters of the coherent point drift algorithm.
+
+The code that performs CPD here is from the pycpd package by Siavash Khallaghi. I copy pasted
+it here instead of importing to reduce dependencies and also to prepare for using a Rust
+implementation in the future.
+"""
